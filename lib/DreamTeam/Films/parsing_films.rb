eb9805cd058096520daad9e7afb3b7d69954e293
имя фамилияДаша <dkozlikina@gmail.com>
2022-10-16 14:44:28 +0300

    start parsing_films

--- lib/DreamTeam/Films/parsing_films.rb
+++ lib/DreamTeam/Films/parsing_films.rb
@@ -2,9 +2,23 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-uri = 'https://rawg.io/'
+uri = 'https://imdb-api.com/en/API/Top250Movies/k_uq4za59n'
 doc = Nokogiri::HTML(URI.open(uri))
 puts doc
+
+# itemes [  {}, {}, {}  ]
+
+#{"items": [ {hash, hash, hash}, {hash, hash, hash}]
+
+# {"id":"tt0111161",
+# "rank":"1",
+# "title":"The Shawshank Redemption",
+# "fullTitle":"The Shawshank Redemption (1994)",
+# "year":"1994",
+# "image":"https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,12,128,176_AL_.jpg",
+# "crew":"Frank Darabont (dir.), Tim Robbins, Morgan Freeman","imDbRating":"9.2","imDbRatingCount":"2650504"}
+
+
 =begin
 showings = []
 doc.css('.showing').each do |showing|
@@ -27,3 +41,4 @@ end
 
 puts JSON.pretty_generate(showings)
 =end
+
