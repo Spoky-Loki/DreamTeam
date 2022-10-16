4b584940fcb253d87d81c78431cbeb5a37df2991
Kertalo <koksharov-kir@yandex.ru>
2022-10-16 18:50:50 +0300

    got id from all the games

--- lib/DreamTeam/Games/parsing_games.rb
+++ lib/DreamTeam/Games/parsing_games.rb
@@ -2,27 +2,34 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-uri = 'https://rawg.io/'
-doc = Nokogiri::HTML(URI.open(uri))
-puts doc
-=begin
-showings = []
-doc.css('.showing').each do |showing|
-  showing_id = showing['id'].split('_').last.to_i
-  tags = showing.css('.tags a').map { |tag| tag.text.strip }
-  title_el = showing.at_css('h1 a')
-  title_el.children.each { |c| c.remove if c.name == 'span' }
-  title = title_el.text.strip
-  dates = showing.at_css('.start_and_pricing').inner_html.strip
-  dates = dates.split('<br>').map(&:strip).map { |d| DateTime.parse(d) }
-  description = showing.at_css('.copy').text.gsub('[more...]', '').strip
-  showings.push(
-    id: showing_id,
-    title: title,
-    tags: tags,
-    dates: dates,
-    description: description
-  )
+def GetGamesID(data)
+  arr = data.match(/<body>(?<information>.*)<\/body>/)[1].split(',')
+  res = []
+  i = 0
+  while i < arr.size
+    a1 = arr[i].match(/\{"appid":(?<id>.*)/)
+    if (!a1.nil?)
+      res.push(a1[:id])
+    end
+    i += 1
+  end
+  return res
 end
-puts JSON.pretty_generate(showings)
+
+=begin
+url2 = 'https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=1440360'
+doc2 = Nokogiri::HTML5(URI.open(url2)).to_s
+#puts doc2
+
+reg = /<body>(?<information>.*)<\/body>/
+match_data = doc2.match(reg)
+
+if match_data.nil?
+  puts "No match"
+else
+  puts(match_data[:information])
+end
 =end
+
+gamesID = GetGamesID(Nokogiri::HTML5(URI.open('https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=32B040E9C3CFEE31A743F7F0FF906454&steamids=76561198109538094')).to_s)
+puts gamesID
