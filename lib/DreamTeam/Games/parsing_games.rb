2ded406d2faaf279e9a609b721105585afd2f789
Kertalo <koksharov-kir@yandex.ru>
2022-10-17 18:45:55 +0300

    get basic information for gamesInformation

--- lib/DreamTeam/Games/parsing_games.rb
+++ lib/DreamTeam/Games/parsing_games.rb
@@ -2,8 +2,8 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-def GetGamesID(min = 0, max = 0)
-  url = Nokogiri::HTML5(URI.open('https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=32B040E9C3CFEE31A743F7F0FF906454&steamids=76561198109538094')).to_s
+def GetGamesID(max = 100)
+  url = Nokogiri::HTML5(URI.open('https://api.steampowered.com/IStoreService/GetAppList/v1/?key=FB8E10E3C18DFD06F605ACF4D049866A&include_games=true&max_results=' + max.to_s)).to_s
   arr = url.match(/<body>(?<information>.*)<\/body>/)[1].split(',')
   res = []
   i = 0
@@ -17,8 +17,7 @@ def GetGamesID(min = 0, max = 0)
     end
     i += 1
   end
-  max = res.size
-  return res[min, max]
+  return res
 end
 
 def InformationOfGames(gamesID)
@@ -27,10 +26,11 @@ def InformationOfGames(gamesID)
     begin
       information = Nokogiri::HTML5(URI.open('https://store.steampowered.com/api/appdetails/?appids=' + g)).to_s
       if information.include?('"type":"game"')
-        #puts information
-        matchInformation = information.match(/"name":"(?<title>.*)","steam_appid".*"short_description":"(?<shortDesc>.*)","supported_languages":"/)
-        if !matchInformation[:title].nil? and !matchInformation[:shortDesc].nil?
-          puts matchInformation[:title] + matchInformation[:shortDesc]
+        #puts information.split(',')
+        informations = information.match(/me":"(?<name>.*)","steam.*t_description":"(?<desc>.*)","sup.*l_formatted":"(?<price>.*)."},"pac.*:{"total":(?<players>.*)},"rel.*date":"(?<data>.*)"},"su/)
+        if !informations[:name].nil? and !informations[:desc].nil?
+          res.push({name:informations[:name], price:informations[:price], players: informations[:players], data: informations[:data], desc: informations[:desc]})
+          #puts informations[:name] + ' ' + informations[:price] + ' ' + informations[:recommend]
         end
       end
     rescue
@@ -40,5 +40,6 @@ def InformationOfGames(gamesID)
   return res
 end
 
-gamesID = GetGamesID(100, 200)
+gamesID = GetGamesID(10)
 gamesInformation = InformationOfGames(gamesID)
+puts gamesInformation
