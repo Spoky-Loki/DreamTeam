8f60b28658ecd692e384f3974be6c8b7ec3d4fde
Kertalo <koksharov-kir@yandex.ru>
2022-10-22 18:51:08 +0300

    change fuctions for users

--- lib/DreamTeam/Games/parsing_games.rb
+++ lib/DreamTeam/Games/parsing_games.rb
@@ -1,33 +1,32 @@
 require 'open-uri'
 require 'nokogiri'
-require 'json'
+require_relative 'api_key'
 
-
-def GetGamesID(max = 100)
-  key = File.open('lib/DreamTeam/Games/key.txt')
-  url = Nokogiri::HTML5(URI.open('https://api.steampowered.com/IStoreService/GetAppList/v1/?key=' +
-                                   + key.read + '&include_games=true&max_results=' + max.to_s)).to_s
+def GetUserGames(steam_id)
+  url = Nokogiri::HTML5(URI.open('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?steamid=' +
+    + steam_id + '&key=' + GetKey())).to_s
   arr = url.split(',')
   res = []
-  i = 0
-  while i < arr.size
-    id = arr[i].match(/\{"appid":(?<id>.*)/)
+  last_id = ''
+  arr.each do |a|
+    id = a.match(/"appid":(?<id>.*)/)
     if !id.nil?
-      res.push(id[:id])
+      last_id = id[:id]
+    else
+      time = a.match(/"playtime_forever":(?<time>.*)/)
+      if !time.nil?
+        res.push({id: last_id, time: time[:time]})
+      end
     end
-    if max != -1 and res.size == max
-      break
-    end
-    i += 1
   end
-  return res
+  res
 end
 
 def InformationOfGames(gamesID)
   res = []
   gamesID.each do |g|
     begin
-      url = Nokogiri::HTML5(URI.open('https://store.steampowered.com/api/appdetails/?appids=' + g)).to_s
+      url = Nokogiri::HTML5(URI.open('https://store.steampowered.com/api/appdetails/?appids=' + g[:id])).to_s
     rescue
       next
     end
@@ -35,6 +34,7 @@ def InformationOfGames(gamesID)
     if name == nil
       next
     end
+    time = g[:time]
     date = url[/date":"(?<date>.*)"},"su/, 1]
     price = url[/nal_formatted":"(?<price>.*)."},"pac/, 1]
     developers = url[/"developers":\[(?<developers>.*)\],"publishers"/, 1]
@@ -45,13 +45,12 @@ def InformationOfGames(gamesID)
     desc = url[/t_description":"(?<desc>.*)","sup/, 1]
     img = url[/"header_image":"(?<img>.*)","website"/, 1]
 
-    res.push({name: name, date: date, price: price, developers: developers,
+    res.push({name: name, time: time, date: date, price: price, developers: developers,
               recommendations: recommendations, desc: desc, img: img})
   end
-  return res
+  res
 end
 
-gamesID = GetGamesID(100)
-gamesInformation = InformationOfGames(gamesID)
+gamesInformation = InformationOfGames(GetUserGames('76561198109538094'))
 puts gamesInformation
 puts gamesInformation.size
